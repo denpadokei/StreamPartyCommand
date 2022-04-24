5516f40c110897ca0683a55739151c74f552a22c
denpadokei <55026301+denpadokei@users.noreply.github.com>
2022-04-24 16:36:55 +0900

    改行削除

--- StreamPartyCommand/Utilities/ColorUtil.cs
+++ StreamPartyCommand/Utilities/ColorUtil.cs
@@ -8,11 +8,9 @@ namespace StreamPartyCommand.Utilities
     public class ColorUtil
     {
         public static ReadOnlyDictionary<string, Color> Colors { get; }
-
         static ColorUtil()
         {
             var dic = new Dictionary<string, Color>();
-            
             foreach (var colorProp in typeof(Color).GetProperties(BindingFlags.Public | BindingFlags.Static)) {
                 var color = colorProp.GetValue(typeof(Color));
                 if (color is Color value) {
