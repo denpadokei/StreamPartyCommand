a3d5fd7370a246f2628033ba35f083c820c73ace
デンパ時計 <jsoldenpa@yahoo.co.jp>
2022-05-07 17:24:41 +0900

    color event

--- StreamPartyCommand/HarmonyPathches/GetNormalColorPatch.cs
+++ StreamPartyCommand/HarmonyPathches/GetNormalColorPatch.cs
@@ -7,16 +7,27 @@ namespace StreamPartyCommand.HarmonyPathches
     [HarmonyPatch(typeof(LightSwitchEventEffect), nameof(LightSwitchEventEffect.GetNormalColor), new Type[] { typeof(int), typeof(bool) })]
     public class GetNormalColorPatch
     {
-        public static void Postfix(ref int beatmapEventValue, ref bool colorBoost, ref Color __result, LightSwitchEventEffect __instance)
+        public static void Postfix(ref int beatmapEventValue, ref bool colorBoost, ref Color __result)
         {
             if (!Enable) {
                 return;
             }
-            if (!__instance.IsColor0(beatmapEventValue)) {
-                __result = LeftColor;
-            }
-            else {
-                __result = RightColor;
+            switch (BeatmapEventDataLightsExtensions.GetLightColorTypeFromEventDataValue(beatmapEventValue)) {
+                case EnvironmentColorType.Color0:
+                    if (!colorBoost) {
+                        __result = LeftColor;
+                    }
+                    break;
+                case EnvironmentColorType.Color1:
+                    if (!colorBoost) {
+                        __result = RightColor;
+                    }
+                    break;
+                case EnvironmentColorType.ColorW:
+                    break;
+                default:
+                    __result = LeftColor;
+                    break;
             }
         }
         internal static bool Enable { get; set; }
